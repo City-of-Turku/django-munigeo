cfbddf1cfcbbabc9d042445c5238106c55ca6f46
ezkat <50319957+ezkat@users.noreply.github.com>
2023-01-24 10:52:51 +0200

    Proper version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-munigeo-turku',
-    version='tku-v0.3',
+    version='0.3',
     packages=['munigeo'],
     include_package_data=True,
     license='BSD License',
